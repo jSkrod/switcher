f5c6e28091787c7b7ec8e4659fe28c5b07ee8208
jSkrod <jedrzej.skrodzki@gmail.com>
2017-09-12 12:51:40 +0200

    Result now also display values after decimal point.

--- src/Switcher.php
+++ src/Switcher.php
@@ -6,17 +6,24 @@ class Switcher {
 
 	public function convert( $number ) {
 		$words = '';
-		$this->prepareInput( $number );
+		// Break number into 'before decimal' and 'after decimal parts';
+		$number = explode( '.', $number );
+
+		$this->prepareInput( $number[0] );
 		foreach ( $this->digits_array as $key => $digits ) {
 			$group = new Group( $digits, $key );
 			$words = $group->convert() . ' ' . $words;
 		}
-		return $words;
+
+		return $this->appendRest( $words, $number[1] );
+	}
+
+	private function appendRest( $words, $rest ) {
+		return $words . ' ' . $rest . '/100';
 	}
 
 	private function prepareInput( $number ) {
 		$digits_array = str_split( $number );
-
 		// Fill array with 0's until we have 3 digits groups.
 		while ( count( $digits_array ) % 3 ) {
 			array_unshift( $digits_array, '0' );
